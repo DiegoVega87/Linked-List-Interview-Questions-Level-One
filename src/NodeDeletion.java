7c4dd8f87f896920886380cb5d1692457c1c3fd1
Diego Vega <diegovega@me.com>
2023-07-16 10:20:15 +0200

    Add examples

--- src/NodeDeletion.java
+++ src/NodeDeletion.java
@@ -5,6 +5,15 @@ public class NodeDeletion {
     * need to delete a node in this list due to data being outdated or irrelevant. Write a function that given
     * a node in the list and the list head, deletes that node in an efficient manner and returns the updated list.
     *
+    * Example 1:
+    *   node = 3 -> 4 -> 5
+    *   head = 1 -> 2 -> 3 -> 4 -> 5
+    *   Output = head = 1 -> 2 -> 3 -> 4 -> 5
+    *
+    * Example 2:
+    *   node = 2 -> 3 -> 4
+    *   node = 2 -> 3 -> 4
+    *   Output = 1 ->3-> 4 -> 5
     *
      * */
 }
