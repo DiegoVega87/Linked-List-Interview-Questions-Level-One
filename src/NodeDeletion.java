0ef94e272b0bb131c3e16efcffb07963bc2fce2d
Diego Vega <diegovega@me.com>
2023-07-16 10:18:01 +0200

    Add problem explanation

---
+++ src/NodeDeletion.java
@@ -0,0 +1,10 @@
+public class NodeDeletion {
+
+    /*
+    * Your startup has built a linked list to store important client and financial data. However, sometimes you may
+    * need to delete a node in this list due to data being outdated or irrelevant. Write a function that given
+    * a node in the list and the list head, deletes that node in an efficient manner and returns the updated list.
+    *
+    *
+     * */
+}
