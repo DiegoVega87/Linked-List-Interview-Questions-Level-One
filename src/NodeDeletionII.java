a125cf90cd137c51c3a6701460722b8bf95c0a4f
Diego Vega <diegovega@me.com>
2023-07-18 10:47:12 +0200

    Add problem explanation

---
+++ src/NodeDeletionII.java
@@ -0,0 +1,11 @@
+public class NodeDeletionII {
+
+    /*
+    * As our company's user base is growing rapidly, we want to optimize the performance of our application.
+    * One potential bottleneck is the time it takes to remove the nth node from the end of a linked list.
+    * We need a function that takes the head of a linked list and the position of the node to remove from the end,
+    * and returns the head of the updated linked list.
+    *
+    *
+    * */
+}
