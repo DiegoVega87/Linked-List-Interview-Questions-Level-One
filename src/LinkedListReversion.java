4b931695db458091db766e19f8cd885889c6d6f6
Diego Vega <diegovega@me.com>
2023-07-18 12:47:16 +0200

    Add problem explanation

---
+++ src/LinkedListReversion.java
@@ -0,0 +1,10 @@
+public class LinkedListReversion {
+
+    /*
+    * Our startup has a feature that allows users to create and manipulate linked lists. We have noticed that some
+    * users are having trouble reversing their linked lists. We want you to implement a function that takes the head
+    * of a singly linked list and reverses the list, returning the reversed list. The function should take the head
+    * node as input and should be able to handle empty lists.
+    *
+    * */
+}
