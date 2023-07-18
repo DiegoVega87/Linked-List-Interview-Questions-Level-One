81627eb6e9ed155111ad0989a05c61c99709f401
Diego Vega <diegovega@me.com>
2023-07-18 18:24:03 +0200

    Add examples

--- src/ListMerging.java
+++ src/ListMerging.java
@@ -5,5 +5,28 @@ public class ListMerging {
     * these lists into one sorted linked list to streamline our analysis process and improve efficiency.
     * Your task is to write a function that takes in the heads of both lists and merges them into one sorted
     * inked list. The function should return the head of the merged linked list.
+    *
+    * Example 1:
+    *   ListNode l1 = new ListNode(1);
+    *   l1.next = new ListNode(3);
+    *   l1.next.next = new ListNode(5);
+    *
+    *   ListNode l2 = new ListNode(2);
+    *   l2.next = new ListNode(4);
+    *   l2.next.next = new ListNode(6);
+    *
+    *   mergedList should be: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+    *
+    * Example 2:
+    *   ListNode l1 = new ListNode(1);
+    *   l1.next = new ListNode(2);
+    *   l1.next.next = new ListNode(3);
+    *
+    *   ListNode l2 = new ListNode(4);
+    *   l2.next = new ListNode(5);
+    *   l2.next.next = new ListNode(6);
+    *
+    *   MergedList should be: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+    *
      * */
 }
