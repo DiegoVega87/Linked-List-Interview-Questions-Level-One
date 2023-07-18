efa90adf1eca972ef3072371caadaedd112e8a0d
Diego Vega <diegovega@me.com>
2023-07-18 18:21:09 +0200

    Add problem explanation

---
+++ src/ListMerging.java
@@ -0,0 +1,9 @@
+public class ListMerging {
+
+    /*
+    * At our startup, we have two sorted linked lists containing customer transaction data. We need to merge
+    * these lists into one sorted linked list to streamline our analysis process and improve efficiency.
+    * Your task is to write a function that takes in the heads of both lists and merges them into one sorted
+    * inked list. The function should return the head of the merged linked list.
+     * */
+}
