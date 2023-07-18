ccf4b28cbe8eb4a5638f86996af3207c90924ae9
Diego Vega <diegovega@me.com>
2023-07-18 12:43:47 +0200

    Add test cases and print method

--- src/NodeDeletionII.java
+++ src/NodeDeletionII.java
@@ -20,7 +20,33 @@ public class NodeDeletionII {
     *
     * */
 
+    public static void main(String[] args){
 
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println("Print the list");
+        printList(node1);
+        System.out.println("Remove node 4");
+        printList(removeNthFromEnd(node1, 2));
+        System.out.println();
+        System.out.println("Remove node 5");
+        printList(removeNthFromEnd(node5, 1));
+        System.out.println();
+        printList(node4);
+        System.out.println();
+        System.out.println("Remove node 4");
+        printList(removeNthFromEnd(node4, 1));
+
+
+
+    }
     public static ListNode removeNthFromEnd(ListNode head, int n){
 
         if(n == 0 || head == null){
@@ -61,5 +87,12 @@ public class NodeDeletionII {
 
     }
 
+    public static void printList(ListNode head){
 
+        if(head == null){
+            return;
+        }
+        System.out.print(head.val + "--> ");
+        printList(head.next);
+    }
 }
