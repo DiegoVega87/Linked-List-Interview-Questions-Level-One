38671bb0f2b4c24f971142d9f273e0dc67dee2a8
Diego Vega <diegovega@me.com>
2023-07-19 13:15:15 +0200

    Add solution and tests

--- src/DetectCyclicWorkFlows.java
+++ src/DetectCyclicWorkFlows.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+
+
 public class DetectCyclicWorkFlows {
 
     /*
@@ -15,6 +18,53 @@ public class DetectCyclicWorkFlows {
     * Example 2:
     *   head = 1 -> 2 -> 3 -> 4 -> 5
     *   Output: false
-     * */
+    * */
 
+    public static void main(String[] args){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        //node5.next = node2;
+
+        System.out.println(hasCycle(node1));
+
+    }
+
+    public static boolean hasCycle(ListNode head){
+
+        if(head == null || head.next == null){
+            return false;
+        }
+
+        HashMap<Integer, Boolean> checker = new HashMap<>();
+
+        while (head != null){
+            boolean visited = checker.getOrDefault(head.val, false);
+
+            if(visited){
+                return true;
+            }else{
+                checker.put(head.val, true);
+            }
+
+            head = head.next;
+        }
+
+        return false;
+    }
+
+    public static void printList(ListNode head){
+
+        if(head == null){
+            return;
+        }
+        System.out.print(head.val + "--> ");
+        printList(head.next);
+    }
 }
