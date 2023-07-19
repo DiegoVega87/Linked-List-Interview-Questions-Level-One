b5c209969c315b4937a21877d150ca8a83c5575c
Diego Vega <diegovega@me.com>
2023-07-19 12:43:56 +0200

    Add problem explanation

---
+++ src/LListPalindromeChecker.java
@@ -0,0 +1,10 @@
+public class LListPalindromeChecker {
+
+    /*
+    * A fintech startup has a website where users can create accounts and enter their personal information.
+    * The company wants to ensure that their platform is secure by implementing a function that checks
+    * if a user's personal information stored in a singly linked list is a palindrome. Write a function that
+    * takes the head of the singly linked list and returns true if the linked list is a palindrome, or false otherwise.
+    *
+    * */
+}
