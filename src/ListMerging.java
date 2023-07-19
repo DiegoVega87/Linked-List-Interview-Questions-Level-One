f659728b770497b3f0534e9bd2a353ca6f6240f9
Diego Vega <diegovega@me.com>
2023-07-19 11:41:19 +0200

    Add solution

--- src/ListMerging.java
+++ src/ListMerging.java
@@ -29,4 +29,67 @@ public class ListMerging {
     *   MergedList should be: 1 -> 2 -> 3 -> 4 -> 5 -> 6
     *
      * */
+
+    public static ListNode mergeSortedLists(ListNode l1, ListNode l2){
+
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+
+        ListNode l3 = null;
+        ListNode tail = null;
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
+
+        while(ptr1 != null || ptr2 != null){
+
+            assert ptr1 != null;
+            if(ptr1.val >= ptr2.val){
+
+                if(l3 == null){
+                    l3 = ptr1;
+                }else{
+                    if(tail == null){
+                        tail = ptr1;
+                        l3.next = tail;
+                    }else{
+                        tail.next = ptr1;
+                        tail = tail.next;
+                    }
+                }
+                ptr1 = ptr1.next;
+            }else{
+
+                if(l3 == null){
+                    l3 = ptr2;
+                }else{
+                    if(tail == null){
+                        tail = ptr2;
+                        l3.next = tail;
+                    }else{
+                        tail.next = ptr2;
+                        tail = tail.next;
+                    }
+                }
+            }
+
+            ptr2 = ptr2.next;
+        }
+
+        while(ptr1 != null){
+            tail.next = ptr1;
+            tail = tail.next;
+            ptr1 = ptr1.next;
+        }
+        while(ptr2 != null){
+            tail.next = ptr2;
+            tail = tail.next;
+            ptr2 = ptr2.next;
+        }
+
+        return l3;
+    }
 }
