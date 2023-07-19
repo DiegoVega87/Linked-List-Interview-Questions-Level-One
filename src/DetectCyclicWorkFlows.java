1638011638af1f7bf00d4e2f84417424664b94cb
Diego Vega <diegovega@me.com>
2023-07-19 12:56:58 +0200

    Add problem explanation

---
+++ src/DetectCyclicWorkFlows.java
@@ -0,0 +1,13 @@
+public class DetectCyclicWorkFlows {
+
+    /*
+    * Our startup has a workflow management system that involves a series of linked tasks.
+    * We suspect that some of these workflows have cycles, which can cause serious operational issues.
+    * We need you to write a function that takes in the head of a workflow and determines whether, or not
+    * a cycle exists within it. Your function should return a boolean value indicating the presence or absence
+    * of the cycle. A cycle is present if there is some node in the list that can be revisited by traversing
+    * the next pointer.
+    *
+    *
+     * */
+}
