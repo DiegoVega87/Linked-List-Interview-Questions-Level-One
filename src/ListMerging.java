f03a86640fbdb561c0489b3c9a8cea1bfd88b1c5
Diego Vega <diegovega@me.com>
2023-07-19 11:54:46 +0200

    Add test cases

--- src/ListMerging.java
+++ src/ListMerging.java
@@ -30,6 +30,33 @@ public class ListMerging {
     *
      * */
 
+    public static void main(String[] args){
+
+        //Test 1:
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        ListNode mergedList = mergeSortedLists(l1, l2);
+
+
+        //Test2
+        l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+
+        l2 = new ListNode(4);
+        l2.next = new ListNode(5);
+        l2.next.next = new ListNode(6);
+
+        mergedList = mergeSortedLists(l1, l2);
+
+    }
+
     public static ListNode mergeSortedLists(ListNode l1, ListNode l2){
 
         if(l1 == null){
