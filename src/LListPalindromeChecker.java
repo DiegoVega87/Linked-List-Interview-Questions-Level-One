e6c10f9bdd867812c8feaec06a5119384e4d4b7b
Diego Vega <diegovega@me.com>
2023-07-19 12:44:56 +0200

    Add examples

--- src/LListPalindromeChecker.java
+++ src/LListPalindromeChecker.java
@@ -6,5 +6,12 @@ public class LListPalindromeChecker {
     * if a user's personal information stored in a singly linked list is a palindrome. Write a function that
     * takes the head of the singly linked list and returns true if the linked list is a palindrome, or false otherwise.
     *
+    * Example 1:
+    *   Input: 1 -> 2 -> 3 -> 2 -> 1
+    *   Output: true
+    *
+    * Example 2:
+    *   Input: 1 -> 2 -> 3 -> 4 -> 5
+    *   Output: false
     * */
 }
